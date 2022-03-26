dd51c7aaf75c3ea3be9c7c69e2e0f86a698c5215
abhibp1993 <abhi.bp1993@gmail.com>
2022-03-26 19:33:58 -0400

    #add: TEntity cpptest

--- ggcore/main.cpp
+++ ggcore/main.cpp
@@ -5,16 +5,26 @@
 #include "types.h"
 #include "version.h"
 #include "entity.h"
-#include "graph.h"
-#include "game.h"
-#include "oneplayer.h"
-#include "mdp.h"
-#include "dtptb.h"
-#include "stptb.h"
+//#include "graph.h"
+//#include "game.h"
+//#include "oneplayer.h"
+//#include "mdp.h"
+//#include "dtptb.h"
+//#include "stptb.h"
 
 using namespace ggsolver;
 
-int main(){
-    PNode node = std::make_shared<TNode>(0);
 
+void test_entity() {
+    TEntity ent0;
+    ent0.set_attr<std::nullptr_t>("nullptr", std::nullptr_t());
+    ent0.set_attr<bool>("bool", true);
+    ent0.set_attr<std::string>("string", "entity");
+    ent0.set_attr<json>("json", {{"ele1", 10}, {"ele2", "string"}});
+}
+
+int main(){
+
+    test_entity();
+    return 0;
 }
