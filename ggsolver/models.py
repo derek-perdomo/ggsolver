0a64289558ae877064e26f2c1bb08fb052ba817e
abhibp1993 <abhi.bp1993@gmail.com>
2022-07-26 19:53:37 -0400

    #wip: commit before complete refactor.

--- ggsolver/models.py
+++ ggsolver/models.py
@@ -2,8 +2,9 @@
 # TODO. Add logging statements.
 # TODO. Make note that user will not be warned of any private attributes that are unserialized.
 # TODO. Make note that user must update RESERVED_PROPERTIES and _graphify_unpointed appropriately.
+# TODO. Instead of registering property function, register just name and then use getattr to
+#  find the appropriate function during serialization. This will allow rebinding functions.
 import inspect
-
 from ggsolver import util
 from ggsolver.graph import NodePropertyMap, EdgePropertyMap, Graph
 
