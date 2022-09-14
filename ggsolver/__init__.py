ef16a7126a9e7f566616d09768d008cb0f9ed281
Abhishek Kulkarni <abhi.bp1993@gmail.com>
2022-09-14 09:37:17 -0400

    Added changelog to ggsolver __init__.py

--- ggsolver/__init__.py
+++ ggsolver/__init__.py
@@ -1,1 +1,9 @@
-__version__ = "0.1.1"
+__version__ = "0.1.2"
+
+
+CHANGELOG = {
+    "0.1.2": [
+        "Added new docker latest, devel images. Updated installation instructions in documentation.",
+        "Added `is_isomorphic_to` function to Graph class. Updated docs and example.",
+    ]
+}
