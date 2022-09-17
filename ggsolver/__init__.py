4f385da814448172a998a349a2812f01767a7b49
abhibp1993 <abhi.bp1993@gmail.com>
2022-09-17 15:23:28 -0400

    #update: changelog and ggsolver version

--- ggsolver/__init__.py
+++ ggsolver/__init__.py
@@ -1,9 +1,17 @@
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 
 CHANGELOG = {
     "0.1.2": [
         "Added new docker latest, devel images. Updated installation instructions in documentation.",
         "Added `is_isomorphic_to` function to Graph class. Updated docs and example.",
-    ]
+    ],
+    "0.1.3": [
+        "Added `logic.pl` module with `simplify, evaluate, to_plformula` and `all_sat` methods for PL formulas.",
+        "Moved `sigma` method to `Automaton` class with default implementation.",
+        "Fixed the representation of `Automaton` acceptance conditions.",
+        "Added new module: `automata`, with `DFA, Monitor, DBA, DCBA, DPA` classes. Docs and example added.",
+        "Automaton can be constructed by passing components (Q, AP, Trans, q0, F) to the constructor.",
+    ],
+
 }
