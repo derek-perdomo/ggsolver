3a357849d6f09c2cc6cf106c7a06a5b0e1fbac8d
Matthew Cohen <cohen.matthew@ufl.edu>
2022-09-24 14:10:32 -0400

    Added rank functions and updated win1_act and win2_act to use rank and out_edges functions

--- ggsolver/dtptb/reachability.py
+++ ggsolver/dtptb/reachability.py
@@ -22,6 +22,9 @@ class SWinReach(Solver):
         self._turn = self._graph["turn"]
         self._final = set(final) if final is not None else {n for n in graph.nodes() if self._graph["final"][n]}
         self._attr = list()
+        # _ranks is indexed by rank and _node_ranks is indexed by node
+        self._ranks = dict()
+        self._node_ranks = dict()
 
     def solve(self):
         # Create initial attractor list with final states
@@ -32,9 +35,6 @@ class SWinReach(Solver):
             pre_1 = set()
             pre_2 = set()
             for node in current_win:
-                # TODO (MC). You need to use self._graph here.
-                #   The call to super() constructor in __init__() function defines self._graph. Check it out.
-                #   In PyCharm, you can hold CTRL and click on a function to jump to that function implementation.
                 predecessors = self._graph.predecessors(node)
                 # Check all predecessors of nodes in current winning region
                 for pred in predecessors:
@@ -44,10 +44,7 @@ class SWinReach(Solver):
                     # Add all nodes where turn=2 and edges only point to winning region
                     if self._graph["turn"][pred] == 2:
                         successors = self._graph.successors(pred)
-                        # TODO (MC). A more efficient way to write the same is as follows:
-                        #   if len(set(successors) - current_win) == 0
-                        #   Q. Can you tell me why?
-                        if set(successors).union(current_win) == current_win:
+                        if len(set(successors) - current_win) == 0:
                             pre_2.add(pred)
             # Add a new attractor that is the union of these two sets and last attractor
             new_attr = current_win.union(pre_1).union(pre_2)
@@ -58,6 +55,7 @@ class SWinReach(Solver):
                 self._attr.append(new_attr)
         self._win1 = set(self._attr[-1])
         self._win2 = set(self._graph.nodes()).difference(set(self._attr[-1]))
+        self.determine_ranks()
 
     def pi1(self, node):
         return self.pick_action(self.win1_act, node)
@@ -66,44 +64,36 @@ class SWinReach(Solver):
         return self.pick_action(self.win2_act, node)
 
     def win1_act(self, node):
-        # TODO (MC). You can break this algorithm using a helper function. Here's some tips, we'll discuss more when we
-        #   meet.
-        #   Q1. What is the key piece of information you need about `node` to decide which of the actions are winning?
-        #   Hint. Visualize where `node` lies in the level set self._attr.
-
-        # TODO (MC). Check out Graph.out_edges() function. It might help you skip `get_actions_from_node()`.
-        if node in final_states:
+        # If final state or non-winning state or not P1s turn at state return empty list
+        if self.rank(node) == 0 or self.rank(node) == float("inf") or self._graph["turn"][node] == 2:
             return list()
-        target_nodes = list()
-        successors = set(self._graph.successors(node))
-        # Check if we are in the winning region, if so use the attractors to find the best action
-        if node in self._win1 and graph["turn"][node] == 1:
-            for attractor in self._attr:
-                if set(attractor).intersection(successors) != set():
-                    target_nodes = list(set(attractor).intersection(successors))
-                    break
-        winning_actions = self.get_actions_from_nodes(node, target_nodes)
+
+        target_nodes = self.nodes_at_rank(self.rank(node)-1)
+        out_edges = self._graph.out_edges(node)
+        winning_actions = list()
+
+        for edge in out_edges:
+            # edge[1] is the node the edge points to
+            if edge[1] in target_nodes:
+                winning_actions.append(edge)
         return winning_actions
 
     def win2_act(self, node):
-        if node in self._win2 and graph["turn"][node] == 2:
-            successors = set(graph.successors(node))
-            target_nodes = self._win2.intersection(successors)
-            winning_actions = self.get_actions_from_nodes(node, target_nodes)
+        if node in self._win2 and self._graph["turn"][node] == 2:
+            out_edges = self._graph.out_edges(node)
+            target_nodes = self._win2
+            winning_actions = list()
+            for edge in out_edges:
+                # edge[1] is the node the edge points to
+                if edge[1] in target_nodes:
+                    winning_actions.append(edge)
             return winning_actions
         else:
             return list()
 
-    def get_actions_from_nodes(self, origin_node, target_nodes):
-        actions = list()
-        for target in target_nodes:
-            actions.append((origin_node, target))
-        return actions
-
     def pick_action(self, winning_function, node):
         winning_actions = winning_function(node)
-        allowable_nodes = graph.successors(node)
-        allowable_actions = self.get_actions_from_nodes(node, allowable_nodes)
+        allowable_actions = self._graph.out_edges(node)
 
         # If there are winning actions
         if len(winning_actions) > 0 and self.strategy_type() == "deterministic":
@@ -117,30 +107,58 @@ class SWinReach(Solver):
             return random.choice(allowable_actions)
 
     def rank(self, node):
-        # TODO (MC). Given the self._attr, assign an integer rank to each state in the self._graph. The rank
-        #  is the level in attractor where the node appears first (nodes in `final` have rank 0. If node is not winning
-        #  for the `player`, then rank is `float("inf")`) .
-        pass
+        """ Returns the rank of a given node """
+        return self._node_ranks[node]
 
+    def nodes_at_rank(self, rank):
+        """ Returns a list of all nodes at a given rank """
+        return self._ranks[rank]
+    def determine_ranks(self):
+        """ Calculates the rank of each node and stores it in self._ranks self._ranks is a dictionary that stores a list
+        of nodes at each rank. Also stores the rank of each node in self._node_ranks. We use two lists so that we can
+        calculate ranks once and then access them in O(1) on average."""
+        ranked_nodes = set()
+        current_rank = 0
+        for attractor in self._attr:
+            for node in attractor:
+                if node not in ranked_nodes:
+                    # Construct _ranks
+                    try:
+                        self._ranks[current_rank].append(node)
+                    except KeyError:
+                        self._ranks[current_rank] = list([node])
+                    # Construct _node_ranks
+                    self._node_ranks[node] = current_rank
+                    ranked_nodes.add(node)
+            current_rank += 1
+        # add nodes not in the attractor list (not winning for P1)
+        for node in self._win2:
+            # Construct _ranks
+            try:
+                self._ranks[float("inf")].append(node)
+            except KeyError:
+                self._ranks[float("inf")] = list([node])
+            # Construct _node_ranks
+            self._node_ranks[node] = float("inf")
 
 ASWinReach = SWinReach
 
 if __name__ == '__main__':
     # Create the graph from richmodels example
-    graph = Graph()
-    s0, s1, s2, s3, s4, s5, s6, s7 = graph.add_nodes(num_nodes=8)
+    graph_rich = Graph()
+    s0, s1, s2, s3, s4, s5, s6, s7 = graph_rich.add_nodes(num_nodes=8)
 
-    graph["turn"] = NodePropertyMap(graph, default=-1)
-    graph["turn"][s0] = 1
-    graph["turn"][s1] = 2
-    graph["turn"][s2] = 2
-    graph["turn"][s3] = 2
-    graph["turn"][s4] = 1
-    graph["turn"][s5] = 2
-    graph["turn"][s6] = 1
-    graph["turn"][s7] = 2
+    graph_rich["turn"] = NodePropertyMap(graph_rich, default=-1)
+    graph_rich["turn"][s0] = 1
+    graph_rich["turn"][s1] = 2
+    graph_rich["turn"][s2] = 2
+    graph_rich["turn"][s3] = 2
+    graph_rich["turn"][s4] = 1
+    graph_rich["turn"][s5] = 2
+    graph_rich["turn"][s6] = 1
+    graph_rich["turn"][s7] = 2
 
-    graph.add_edges([
+    graph_rich.add_edges([
                     (s0, s1), (s0, s3),
                     (s1, s0), (s1, s2), (s1, s4),
                     (s2, s2), (s2, s4),
@@ -153,9 +171,9 @@ if __name__ == '__main__':
     final_states = (s3, s4)
 
     # Test solver
-    solver = SWinReach(graph, final=final_states)
+    solver = SWinReach(graph_rich, final=final_states)
     solver.solve()
-    for n in graph.nodes():
+    for n in graph_rich.nodes():
         print(n, " win1_acts: ", solver.win1_act(n))
         print(n, " win2_acts: ", solver.win2_act(n))
     print(solver.win1())
