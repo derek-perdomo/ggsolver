d495a40f8c4e43fdd9ca6c1f20e38c83abb14cc5
Abhishek Kulkarni <abhi.bp1993@gmail.com>
2022-11-04 22:03:20 -0400

    #add: (pbp asi) Implemented product of MDP and DFPA

--- ggsolver/pbp/asi/models.py
+++ ggsolver/pbp/asi/models.py
@@ -1,4 +1,5 @@
 from ggsolver.logic.prefltl import *
+from ggsolver.mdp.models import QualitativeMDP
 
 
 def assign_rank(dfpa: DFPA):
@@ -23,8 +24,71 @@ def assign_rank(dfpa: DFPA):
     print(pg["rank"])
 
 
+class ProductPrefMDP(QualitativeMDP):
+    def __init__(self, mdp:QualitativeMDP, dfpa:DFPA):
+        super(QualitativeMDP, self).__init__()
+        self._mdp = mdp
+        self._dfpa = dfpa
+        self._pref_graph = self._construct_pref_graph()
+
+    def states(self):
+        return list(itertools.product(self._mdp.states(), self._dfpa.states()))
+
+    def actions(self):
+        return self._mdp.actions()
+
+    def delta(self, state, act):
+        pass
+
+    def pref_graph(self, force_reconstruct=False):
+        if force_reconstruct:
+            self._construct_pref_graph()
+        return self._pref_graph
+
+    def _construct_pref_graph(self):
+        graph = Graph()
+        pg = self._dfpa.pref_graph()
+
+        # Nodes of preference graph have same identifier (MP-outcomes) as that in DFPA's nodes.
+        node_ids = graph.add_nodes(len(pg["state"].keys()))
+
+        np_state = graph["state"] = NodePropertyMap(graph)
+        np_state.update({id_: pg["state"][id_] for id_ in node_ids})
+
+        # Add states in product MDP to each node partition
+        np_partition = graph["partition"] = NodePropertyMap(graph)
+        for id_ in node_ids:
+            dfpa_states = pg["partition"][id_]
+            np_partition[id_] = {(mdp_st, dfpa_st) for mdp_st in self._mdp.states() for dfpa_st in dfpa_states}
+
+        return graph
+
+
 if __name__ == '__main__':
+    mdp_ = QualitativeMDP(
+        states=[f"s{i}" for i in range(8)] + ["sink"],
+        actions=['alpha', 'beta'],
+        trans_dict={
+            "s0": {'alpha': ["s1"], 'beta': ["s2", "s4"]},
+            "s1": {'alpha': ["s1", "s2", "s3"], 'beta': ["sink"]},
+            "s2": {'alpha': ["s2"], 'beta': ["sink"]},
+            "s3": {'alpha': ["s3"], 'beta': ["sink"]},
+            "s4": {'alpha': ["s5", "s6"], 'beta': ["sink"]},
+            "s5": {'alpha': ["s6"], 'beta': ["s2", "s7"]},
+            "s6": {'alpha': ["s5", "s6"], 'beta': ["sink"]},
+            "s7": {'alpha': ["s2", "s3"], 'beta': ["sink"]},
+            "sink": {'alpha': ["sink"], 'beta': ["sink"]},
+        },
+        init_state="s0",
+        final=["s6"]
+        # final=["s6", "s7"]
+        # final=["s2", "s3"]
+    )
+
     formula_ = PrefScLTL("Fa > Fb")
     model_ = formula_.model()
     aut_ = formula_.translate()
     assign_rank(aut_)
+
+    product_mdp_ = ProductPrefMDP(mdp_, aut_)
+    print(product_mdp_.pref_graph()["partition"].items())
